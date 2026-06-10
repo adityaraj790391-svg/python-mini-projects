65b7d87f9f3faec88da6f9cdd66ac3d91e7b9dbf
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-10 23:41:30 +0530

    done

--- RentCalculator.py
+++ RentCalculator.py
@@ -8,12 +8,20 @@
 ## Output
 # Total amount you've to pay is
 
-rent = int(input("Enter the total rent: "))
+rent = int(input("Enter your room/flat rent: "))
 food = int(input("Enter the total food ordered: "))
 electricity = int(input("Enter the electricity units spent: "))
 charge_per_unit = int(input("Enter the charge per unit: "))
-persons = int(input("Enter the number of persons living: "))
+maid = int(input("Enter the maid charge: "))
+persons = int(input("Enter the number of persons living in room/flat: "))
+wifi = int(input("Enter the wifi charge: "))
+maintainance = int(input("Enter the maintenance charge: "))
 
 total_electricity_charge = electricity * charge_per_unit
-total_amount = rent + food + total_electricity_charge
+
+total_amount = rent + food + total_electricity_charge + maid + wifi + maintainance
+
 amount_per_person = total_amount / persons
+
+print("Total amount you've to pay is:" , total_amount)
+print("Amount per person is:" , amount_per_person)
