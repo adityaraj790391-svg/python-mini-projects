e62559d91720fdd3f3f4d55fbeba7614f1cc68fa
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-11 19:57:01 +0530

    done

--- RockPaperScissorGame.py
+++ RockPaperScissorGame.py
@@ -34,11 +34,28 @@ print("Computer's choice:", computer_choice)
 if user_choice == computer_choice:
     print("Both chooses same: Match tie")
 
-elif (user_choice == "Rock" and computer_choice == "Scissors") or (user_choice == "Paper" and computer_choice == "Rock") or (user_choice == "Scissors" and computer_choice == "Paper"):
-    print("User wins")
-else:
-    print("Computer wins")
+# elif (user_choice == "Rock" and computer_choice == "Scissors") or (user_choice == "Paper" and computer_choice == "Rock") or (user_choice == "Scissors" and computer_choice == "Paper"):
+#     print("User wins")
+# else:
+#     print("Computer wins")
 
 
 ## Another way to write the same code is:
 
+elif user_choice == "Rock":
+    if computer_choice == "Scissors":
+        print("Rock break the scissors : User wins")
+    else:
+        print("Paper covers rock : Computer wins")
+
+elif user_choice == 'Paper':
+    if computer_choice == "Rock":
+        print("Paper covers rock : User wins")
+    else:
+        print("Scissors cut paper : Computer wins")
+
+elif user_choice == "Scissors":
+    if computer_choice == "Paper":
+        print("Scissors cut paper : User wins")
+    else:
+        print("Rock break the scissors : Computer wins")
