a78d7cd9d4fa53dc00d3aad1655e51247c62fa6a
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-12 23:00:46 +0530

    done

--- DigitalClock.py
+++ DigitalClock.py
@@ -9,4 +9,8 @@ root.configure(bg="black")
 def time():
     string = time.strftime("%H:%M%S \n %D")
     label.config(text=string)
+    label.after(1000, time)
+
+
+label = tk.Label(root, font=("ds-digital", 50), background="black", foreground="cyan")
 
