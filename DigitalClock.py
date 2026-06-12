f6df5a16147e8ad93734bc15c82e1609a3e686bb
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-12 23:04:04 +0530

    done

--- DigitalClock.py
+++ DigitalClock.py
@@ -7,10 +7,14 @@ root.geometry("400x200")
 root.configure(bg="black")
 
 def time():
-    string = time.strftime("%H:%M%S \n %D")
+    string = time.strftime("%H:%M:%S %p \n %D")
     label.config(text=string)
     label.after(1000, time)
 
 
-label = tk.Label(root, font=("ds-digital", 50), background="black", foreground="cyan")
+label = tk.Label(root, font=("calibri", 50, 'bold'), background="black", foreground="cyan")
+label.pack(anchor='center')
+
+time()
+root.mainloop()
 
