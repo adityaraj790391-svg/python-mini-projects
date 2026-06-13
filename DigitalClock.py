4e29b6ea87e9ae6b2f4f18508854a24d77a355e2
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-13 23:15:27 +0530

    done

--- DigitalClock.py
+++ DigitalClock.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from time import strftime
 
 root = tk.Tk()
-root.title("Digital Clock Application Develop")
+root.title("Digital Clock Application Developed")
 
 def time():
     string = strftime("%H:%M:%S %p \n %D")
