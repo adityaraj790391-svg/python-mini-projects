bd8735ae5425923173d349ca5b94ab10ae3145f5
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-17 23:24:49 +0530

    done

--- TicTacToeGame.py
+++ TicTacToeGame.py
@@ -28,3 +28,6 @@ root = tk.Tk()
 root.title("Tic Tac Toe")
 
 buttons = [tk.Button(root, text="", font=("Arial", 24), width=5, height=2, command=lambda i=i: button_click(i)) for i in range(9)]
+
+for i, button in enumerate(buttons):
+    button.grid(row=i//3, column=i%3)
