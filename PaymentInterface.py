4f954061cb6a3939165f725a3c65fc97302abdf9
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-18 20:03:28 +0530

    done

--- PaymentInterface.py
+++ PaymentInterface.py
@@ -7,3 +7,9 @@ upi_id = input("Enter your UPI ID: ")
 # upi://pay?pa=UPI_ID&pn=NAME&am=Amount&cu=CURRENCY&tn=MESSAGE
 
 # Defning the payment URL based on the UPI ID and the payment app
+# Ypu can modify these URLs based on the payment apps you want to support
+
+phonepe_url = f"upi://pay?pa={upi_id}&pn=YourName&am=10&cu=INR&tn=Payment for services"
+gpay_url = f"upi://pay?pa={upi_id}&pn=YourName&am=10&cu=INR&tn=Payment for services"
+paytm_url = f"upi://pay?pa={upi_id}&pn=YourName&am=10&cu=INR&tn=Payment for services"
+
