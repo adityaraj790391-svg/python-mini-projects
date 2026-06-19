4032a60cd210e979e04dc6a195c537cf60862913
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-19 21:03:09 +0530

    done

--- To-DoApp.py
+++ To-DoApp.py
@@ -1,2 +1,14 @@
 def task():
     tasks = []
+    print("Welcome to the To-Do App!")
+
+    total_task = int(input("Enter the total number of tasks you want to add: "))
+    for i in range(1, total_task + 1):
+        task_name = input(f"Enter task {i}: ")
+        tasks.append(task_name)
+
+
+    print(f"Tasks added successfully! You have added {total_task} tasks.")
+    print(f"Today's tasks are:\n{tasks}")
+
+    while 
