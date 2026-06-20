223bb136175d609ad7ca309ed28aa4f078d2c5c6
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-20 23:20:33 +0530

    done

--- StudentGradeMangementSystem.py
+++ StudentGradeMangementSystem.py
@@ -3,25 +3,38 @@ Dictionary - key + value pair
 left side = key
 right side = value
 {key: value, key: value, key: value}
-"""
 
-"""
+
 1. Add
 2. Update
 3. Delete
 4. View
 5. Exit/Stop
-"""
 
-# Basics 
 
-# Create a dictionary
+Basics 
 
+Create a dictionary
 var = {"name": "Veriel", "name": "Luna", "name": "Zeke"}
 print(var)  
 
-# Accessing an element
+Accessing an element
 print(var["name"])  
 
+Updating an element
+var["name"] = "Zeke"
+print(var)
+
+Deleting an element
+del var["name"]
+print(var)
+
+View
+print(var)  
+
+"""
 
 
+# Initializing an empty dictionary to store student_grades
+
+student_grades = {}
