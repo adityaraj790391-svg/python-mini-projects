33b83aba7d0f6f0567a719b8b9aac4e3da4a2855
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-20 23:52:31 +0530

    done

--- StudentGradeMangementSystem.py
+++ StudentGradeMangementSystem.py
@@ -63,7 +63,57 @@ def delete_student(name):
     else:
         print(f"{name} not found")
 
-# View all students and their grades
+# View all students
 
+def display_all_students():
+    if student_grades:
+        print("Student Grades:")
+        for name, grade in student_grades.items():
+            print(f"{name}: {grade}")
+    else:
+        print("No students found.")
+
+def main():
+    while True:
+        print("\nStudent Grade Management System")
+        print("1. Add Student")
+        print("2. Update Student Grade")
+        print("3. Delete Student")
+        print("4. View All Students")
+        print("5. Exit")
+
+        choice = int(input("Enter your choice = "))
+
+        if choice == 1:
+            name = input("Enter your name = ")
+            grade = int(input("Enter your grade = "))
+            add_student(name, grade)
+
+        elif choice == 2:
+            name = input("Enter your name = ")
+            grade = int(input("Enter your grade = "))
+            update_student(name, grade)
+
+        elif choice == 3:
+            name = input("Enter your name = ")
+            delete_student(name)
+
+        elif choice == 4:
+            display_all_students(name)
+
+        elif choice == 5:
+            print("Closing the program...")
+            break
+
+        else:
+            print("Invalid choice")
+
+main()
+
+            
+
+
+
+           
 
 
