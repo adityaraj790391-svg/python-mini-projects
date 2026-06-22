856fd3f03dc3fc0ced4dac8aa226378c536833b2
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-06-22 21:21:23 +0530

    done

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -1,3 +1,11 @@
 # Step-1 Import the important libraries
 
 from spellchecker import SpellChecker
+
+# Step-2 Creating the app class
+
+class SpellingCheckerApp:
+    def __init__(self):
+        self.spell_checker = SpellChecker()
+
+    
